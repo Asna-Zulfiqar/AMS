402b59d1e52b4bc0c4dbcab182a4690b06b6a7f1
Asna-Zulfiqar <asna.zulfiqar018@gmail.com>
2024-09-23 09:31:51 +0500

    admin.py

--- AMS/company/admin.py
+++ AMS/company/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Accounts , Company , Report , Expense , Income , CompanyUsers
+from .models import Accounts , Company , Expense , Income , CompanyUsers
 
 admin.site.register(Company)
-admin.site.register(Report)
 admin.site.register(Accounts)
 admin.site.register(Expense)
 admin.site.register(Income)
