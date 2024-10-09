68003b286983d20880ea449c12db21cd8d497ed3
Asna-Zulfiqar <asna.zulfiqar018@gmail.com>
2024-10-09 09:33:25 +0500

    Delete AMS/company directory

--- AMS/company/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Accounts , Company , Expense , Income , CompanyUsers
-
-admin.site.register(Company)
-admin.site.register(Accounts)
-admin.site.register(Expense)
-admin.site.register(Income)
-admin.site.register(CompanyUsers)
